99ee951f6b038d67d3d4a093f032afce193d11ac
Slavikus1 <vyacheslav921@icloud.com>
2024-11-09 13:29:25 +0300

    RA-04 поменял метод add

--- app/src/main/java/ru/aliohin/recipesapp/MainActivity.kt
+++ app/src/main/java/ru/aliohin/recipesapp/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.aliohin.recipesapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
 import ru.aliohin.recipesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,11 +16,10 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        if (binding.mainContainer != null){
-            val fragmentManager = supportFragmentManager
-            val fragmentTransaction = fragmentManager.beginTransaction()
-            fragmentTransaction.add(R.id.mainContainer, CategoriesListFragment())
-            fragmentTransaction.commit()
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add(R.id.mainContainer, CategoriesListFragment())
         }
+
     }
 }
