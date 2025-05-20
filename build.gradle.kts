1d2f7f92ffaee2475ad0d08e5762a49f0519eada
Slavikus1 <vyacheslav921@icloud.com>
2025-05-20 23:35:07 +0300

    RA-45 test push 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     id("androidx.navigation.safeargs.kotlin") version libs.versions.safeArgs apply false
-    kotlin("plugin.serialization") version "2.0.21"
+    kotlin("plugin.serialization") version libs.versions.serialization
     id("com.google.devtools.ksp") version libs.versions.ksp apply false
-    id ("com.google.dagger.hilt.android") version libs.versions.hiltVersion apply false
+    id("com.google.dagger.hilt.android") version libs.versions.hiltVersion apply false
 }
